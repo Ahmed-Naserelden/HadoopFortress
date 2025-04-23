230cd8946562f19faf163cb130bb1a0c6aba13b0
Ahmed-Naserelden <an2071497@gmail.com>
2025-04-23 23:20:01 +0200

    Add Dockerfile and entrypoint scripts for Hive and Tez integration, configure environment variables, and set up Tez in HDFS

--- shared/hive/metastore/entrypoint.sh
+++ shared/hive/metastore/entrypoint.sh
@@ -7,14 +7,11 @@ cp /shared/configuration/master/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.
 cp /shared/configuration/master/yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml
 cp /shared/configuration/master/mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml
 cp /shared/configuration/master/hadoop-env.sh $HADOOP_HOME/etc/hadoop/hadoop-env.sh
-cp /shared/hive/hive-site.xml $HIVE_HOME/conf/hive-site.xml
-cp /shared/postgresql-42.2.23.jar $HIVE_HOME/lib/
-cp /shared/tez/tez-site.xml $TEZ_HOME/conf/tez-site.xml
-cp $HADOOP_HOME/share/hadoop/common/lib/guava-27.0-jre.jar $HIVE_HOME/lib
 
 
 
 echo "Creating Tez directory in HDFS..."
+
 hdfs dfs -mkdir -p /apps/tez;
 
 echo "Uploading Tez tez.tar.gz to HDFS..."
@@ -25,12 +22,10 @@ hdfs dfs -chown -R hduser:hadoop /apps/tez; hdfs dfs -chmod -R 755 /apps
 echo "Uploading Tez jar files to HDFS Completed..."
 
 
-cat ~/.bashrc &> /shared/terr.log
-
-echo "Starting Hive services..."
-# Start postgresql service
 schematool -dbType postgres -initSchema
+
 sleep 10
+
 # Initialize Hive metastore
 hive --service metastore &> /shared/${HOSTNAME}_metastore.log &
 
--- shared/hive/server/entrypoint.sh
+++ shared/hive/server/entrypoint.sh
@@ -10,9 +10,7 @@ cp /shared/configuration/master/core-site.xml $HADOOP_HOME/etc/hadoop/core-site.
 cp /shared/configuration/master/yarn-site.xml $HADOOP_HOME/etc/hadoop/yarn-site.xml
 cp /shared/configuration/master/mapred-site.xml $HADOOP_HOME/etc/hadoop/mapred-site.xml
 cp /shared/configuration/master/hadoop-env.sh $HADOOP_HOME/etc/hadoop/hadoop-env.sh
-cp /shared/hive/hive-site.xml $HIVE_HOME/conf/hive-site.xml
-cp /shared/postgresql-42.2.23.jar $HIVE_HOME/lib/
-cp /shared/tez/tez-site.xml $TEZ_HOME/conf/tez-site.xml
+
 cp $HADOOP_HOME/share/hadoop/common/lib/guava-27.0-jre.jar $HIVE_HOME/lib
 
 sleep 12
